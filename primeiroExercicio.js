d33182f05bacbb89794a6732473c3a3a340f34ff
dionisilva51 <sdionatan@ymail.com>
2026-03-25 20:24:03 -0300

    Add files via upload

---
+++ primeiroExercicio.js
@@ -0,0 +1,35 @@
+//exercicio 1
+let num = 3
+let num2 = 4
+
+console.log(num + num2)
+
+//Exercicio 2
+
+let num3 = 5
+
+let num6 = 2
+
+console.log(num * num3 / num6 )
+
+//exercicio 3
+
+let num4 = -1
+let resultado1 = num2 - num3
+
+let resultado2 = resultado1 * num4
+
+console.log(resultado2)
+
+//exercicio 4
+
+let num5 = 234
+
+let resultado3 = num5 % num3
+
+console.log(resultado3)
+
+
+
+
+
---
+++ primeiroExercicio01.js
@@ -0,0 +1,29 @@
+//Exercicio 1
+
+let firstNumber = 3
+let secondNumber = 4
+
+console.log(firstNumber + secondNumber)
+
+//Exercicio 2
+let thirdNumber = 3
+let fourtNumber = 5
+
+console.log((thirdNumber * fourtNumber) / 2)
+
+//Exercicio 3
+
+let fiveNumber = 5
+let sixNumber = 4
+let sevenNumber = -1
+
+console.log((sixNumber - fiveNumber) * sevenNumber)
+// Outra forma de dar o resultado no console.log é:
+console.log((sixNumber - fiveNumber) * -1)
+
+//Exercicio 4
+
+let eightNumber = 234
+let nineNumber = 5
+
+console.log(eightNumber % nineNumber)
