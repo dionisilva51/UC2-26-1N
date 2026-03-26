fdb009c850d95c5ea1c7affd9f0c253f27d0217d
Dionatan Silva <sdionatan@ymail.com>
2026-03-26 19:24:13 -0300

    Add files via upload

---
+++ segundoExercicio.js
@@ -0,0 +1,8 @@
+let oneNumber = 30
+let twoNumber = 20
+
+console.log(oneNumber === twoNumber)
+console.log(oneNumber !== twoNumber)
+console.log(oneNumber > twoNumber)
+console.log(oneNumber < twoNumber)
+
